a024b3d23d72920ebd3235d2bc7796e3fca2ac5d
Blake Winton <bwinton@latte.ca>
2022-08-07 19:36:53 -0400

    Get the deflation working!

--- src/penrose.rs
+++ src/penrose.rs
@@ -1,16 +1,18 @@
 use std::{f32::consts::PI, fmt::Display};
 
-use crate::util::{Algorithm, CELL_WIDTH, COLORS, COLUMNS, LINE_WIDTH, OFFSET, ROWS};
+use crate::util::{Algorithm, CELL_WIDTH, COLORS, COLUMNS, LINE_WIDTH, OFFSET, ROWS, WHITE};
 use maze_utils::From;
 
 use macroquad::{
     logging as log,
-    shapes::{draw_circle, draw_line, draw_rectangle_lines},
+    prelude::Vec2,
+    shapes::{draw_line, draw_rectangle, draw_rectangle_lines, draw_triangle},
 };
 
-const LENGTH: f32 = 50.0;
+const LENGTH: f32 = 300.0;
 lazy_static! {
-    static ref SMALL_LENGTH: f32 = LENGTH * 2.0 / (1.0 + 5.0f32.sqrt());
+    static ref GOLDEN_RATIO: f32 = (1.0 + 5.0f32.sqrt()) / 2.0;
+    static ref SMALL_LENGTH: f32 = LENGTH / *GOLDEN_RATIO;
 }
 
 #[derive(PartialEq, Eq, Debug)]
@@ -20,7 +22,7 @@ enum State {
     Done,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Point(f32, f32);
 
 impl Point {
@@ -31,42 +33,73 @@ impl Point {
     pub(crate) fn polar_to_rect(r: f32, theta: f32) -> Self {
         Self(r * theta.cos(), r * theta.sin())
     }
+
+    fn move_to(&self, a: &Point, ratio: f32) -> Self {
+        let Point(cx, cy) = *self;
+        let Point(ax, ay) = *a;
+        let x = cx + (ax - cx) * ratio;
+        let y = cy + (ay - cy) * ratio;
+        Point(x, y)
+    }
 }
 
-#[derive(Debug)]
-enum Tile {
-    Kite(Point, Point, Point, Point),
-    Dart(Point, Point, Point, Point),
+impl From<Point> for Vec2 {
+    fn from(p: Point) -> Self {
+        Vec2::new(p.0, p.1)
+    }
 }
+
+#[derive(Debug, Copy, Clone)]
+struct Tile(bool, Point, Point, Point);
 
 impl Tile {
     fn draw(&self) {
-        match self {
-            Tile::Kite(a, b, c, d) | Tile::Dart(a, b, c, d) => {
-                draw_line(a.0, a.1, b.0, b.1, LINE_WIDTH, COLORS[2]);
-                draw_line(b.0, b.1, c.0, c.1, LINE_WIDTH, COLORS[2]);
-                draw_line(c.0, c.1, d.0, d.1, LINE_WIDTH, COLORS[2]);
-                draw_line(d.0, d.1, a.0, a.1, LINE_WIDTH, COLORS[2]);
-            }
+        let Tile(is_kite, a, b, c) = *self;
+        let color = if is_kite { COLORS[3] } else { COLORS[4] };
+        draw_line(a.0, a.1, b.0, b.1, LINE_WIDTH, COLORS[2]);
+        draw_line(b.0, b.1, c.0, c.1, LINE_WIDTH, COLORS[2]);
+        // draw_line(c.0, c.1, a.0, a.1, LINE_WIDTH, COLORS[5]);
+        let b2 = c.move_to(&a, 0.5);
+
+        draw_triangle(
+            c.move_to(&a, 0.1).into(),
+            a.move_to(&c, 0.1).into(),
+            b.move_to(&b2, 0.2).into(),
+            color,
+        );
+    }
+
+    fn build_tile(i: f32, x_center: f32, y_center: f32, is_kite: bool) -> [Tile; 2] {
+        let length = if is_kite { LENGTH } else { *SMALL_LENGTH };
+        let a = Point(x_center, y_center);
+        let b = Point::polar_to_rect(LENGTH, i * PI / 10.0).offset(x_center, y_center);
+        let c = Point::polar_to_rect(length, (i + 2.0) * PI / 10.0).offset(x_center, y_center);
+        let d = Point::polar_to_rect(LENGTH, (i + 4.0) * PI / 10.0).offset(x_center, y_center);
+        // [Tile(is_kite, a, b, c), Tile(is_kite, a, b, c)]
+        [Tile(is_kite, a, b, c), Tile(is_kite, a, d, c)]
+    }
+
+    fn subdivide(&self) -> Vec<Tile> {
+        let Tile(is_kite, a, b, c) = *self;
+        if is_kite {
+            // Subdivide half kite triangle
+            // Q = A + (B - A) / goldenRatio
+            let q = b.move_to(&a, 1.0 / *GOLDEN_RATIO);
+            // R = B + (C - B) / goldenRatio
+            let r = a.move_to(&c, 1.0 / *GOLDEN_RATIO);
+            // [(1, R, Q, B), (0, Q, A, R), (0, C, A, R)]
+            vec![
+                Tile(false, a, r, q),
+                Tile(true, b, q, r),
+                Tile(true, b, c, r),
+            ]
+        } else {
+            // Subdivide half dart triangle
+            // P = C + (A - C) / goldenRatio
+            let p = a.move_to(&b, 1.0 / *GOLDEN_RATIO);
+            // [(1, B, P, A), (0, P, C, B)]
+            vec![Tile(true, a, p, c), Tile(false, b, c, p)]
         }
-    }
-
-    fn build_kite(i: f32, x_center: f32, y_center: f32) -> Tile {
-        let a = Point(x_center, y_center);
-        let b = Point::polar_to_rect(LENGTH, i * PI / 10.0).offset(x_center, y_center);
-        let c = Point::polar_to_rect(LENGTH, (i + 2.0) * PI / 10.0).offset(x_center, y_center);
-        let d = Point::polar_to_rect(LENGTH, (i + 4.0) * PI / 10.0).offset(x_center, y_center);
-        Tile::Kite(a, b, c, d)
-    }
-
-    fn build_dart(i: f32, x_center: f32, y_center: f32) -> Tile {
-        let small_length = *SMALL_LENGTH;
-        let a = Point(x_center, y_center);
-        let b = Point::polar_to_rect(LENGTH, i * PI / 10.0).offset(x_center, y_center);
-        let c =
-            Point::polar_to_rect(small_length, (i + 2.0) * PI / 10.0).offset(x_center, y_center);
-        let d = Point::polar_to_rect(LENGTH, (i + 4.0) * PI / 10.0).offset(x_center, y_center);
-        Tile::Dart(a, b, c, d)
     }
 }
 
@@ -104,72 +137,87 @@ impl Variant {
             Variant::Sun => {
                 // Create wheel of red triangles around the origin
                 for i in 0..5 {
-                    tiles.push(Tile::build_kite(i as f32 * 4.0 + 1.0, x_center, y_center));
+                    tiles.extend(Tile::build_tile(
+                        i as f32 * 4.0 + 1.0,
+                        x_center,
+                        y_center,
+                        true,
+                    ));
                 }
             }
             Variant::Star => {
                 for i in 0..5 {
-                    tiles.push(Tile::build_dart(i as f32 * 4.0 - 1.0, x_center, y_center));
+                    tiles.extend(Tile::build_tile(
+                        i as f32 * 4.0 - 1.0,
+                        x_center,
+                        y_center,
+                        false,
+                    ));
                 }
             }
             Variant::Ace => {
-                let tile = Tile::build_kite(1.0, x_center, y_center - LENGTH);
-                tiles.push(tile);
-                let tile = Tile::build_kite(5.0, x_center, y_center - LENGTH);
-                tiles.push(tile);
-                let tile = Tile::build_dart(13.0, x_center, y_center + small_length);
-                tiles.push(tile);
+                let tile = Tile::build_tile(1.0, x_center, y_center - LENGTH, true);
+                tiles.extend(tile);
+                let tile = Tile::build_tile(5.0, x_center, y_center - LENGTH, true);
+                tiles.extend(tile);
+                let tile = Tile::build_tile(13.0, x_center, y_center + small_length, false);
+                tiles.extend(tile);
             }
             Variant::Deuce => {
-                let tile = Tile::build_dart(1.0, x_center, y_center - LENGTH);
-                tiles.push(tile);
-                let tile = Tile::build_dart(5.0, x_center, y_center - LENGTH);
-                tiles.push(tile);
+                let tile = Tile::build_tile(1.0, x_center, y_center - LENGTH, false);
+                tiles.extend(tile);
+                let tile = Tile::build_tile(5.0, x_center, y_center - LENGTH, false);
+                tiles.extend(tile);
                 let offset = Point::polar_to_rect(LENGTH, 1.0 * PI / 10.0);
-                let tile = Tile::build_kite(17.0, x_center - offset.0, y_center + offset.1);
-                tiles.push(tile);
-                let tile = Tile::build_kite(9.0, x_center + offset.0, y_center + offset.1);
-                tiles.push(tile);
+                let tile = Tile::build_tile(17.0, x_center - offset.0, y_center + offset.1, true);
+                tiles.extend(tile);
+                let tile = Tile::build_tile(9.0, x_center + offset.0, y_center + offset.1, true);
+                tiles.extend(tile);
             }
             Variant::Jack => {
-                let tile = Tile::build_kite(11.0, x_center, y_center);
-                tiles.push(tile);
-                let tile = Tile::build_kite(15.0, x_center, y_center);
-                tiles.push(tile);
-                let tile = Tile::build_kite(13.0, x_center, y_center + LENGTH);
-                tiles.push(tile);
+                let tile = Tile::build_tile(11.0, x_center, y_center, true);
+                tiles.extend(tile);
+                let tile = Tile::build_tile(15.0, x_center, y_center, true);
+                tiles.extend(tile);
+                let tile = Tile::build_tile(13.0, x_center, y_center + LENGTH, true);
+                tiles.extend(tile);
 
                 let offset = Point::polar_to_rect(LENGTH, 19.0 * PI / 10.0);
-                let tile = Tile::build_dart(5.0, x_center + offset.0, y_center + offset.1);
-                tiles.push(tile);
-                let tile = Tile::build_dart(1.0, x_center - offset.0, y_center + offset.1);
-                tiles.push(tile);
+                let tile = Tile::build_tile(5.0, x_center + offset.0, y_center + offset.1, false);
+                tiles.extend(tile);
+                let tile = Tile::build_tile(1.0, x_center - offset.0, y_center + offset.1, false);
+                tiles.extend(tile);
             }
             Variant::Queen => {
-                let tile = Tile::build_dart(13.0, x_center, y_center);
-                tiles.push(tile);
+                let tile = Tile::build_tile(13.0, x_center, y_center, false);
+                tiles.extend(tile);
 
                 let offset = Point::polar_to_rect(LENGTH, 17.0 * PI / 10.0);
-                let tile = Tile::build_kite(3.0, x_center + offset.0, y_center + offset.1);
-                tiles.push(tile);
-                let tile = Tile::build_kite(3.0, x_center - offset.0, y_center + offset.1);
-                tiles.push(tile);
+                let tile = Tile::build_tile(3.0, x_center + offset.0, y_center + offset.1, true);
+                tiles.extend(tile);
+                let tile = Tile::build_tile(3.0, x_center - offset.0, y_center + offset.1, true);
+                tiles.extend(tile);
 
-                let tile = Tile::build_kite(11.0, x_center, y_center + LENGTH);
-                tiles.push(tile);
-                let tile = Tile::build_kite(15.0, x_center, y_center + LENGTH);
-                tiles.push(tile);
+                let tile = Tile::build_tile(11.0, x_center, y_center + LENGTH, true);
+                tiles.extend(tile);
+                let tile = Tile::build_tile(15.0, x_center, y_center + LENGTH, true);
+                tiles.extend(tile);
             }
             Variant::King => {
                 for i in 2..5 {
-                    tiles.push(Tile::build_dart(i as f32 * 4.0 + 1.0, x_center, y_center));
+                    tiles.extend(Tile::build_tile(
+                        i as f32 * 4.0 + 1.0,
+                        x_center,
+                        y_center,
+                        false,
+                    ));
                 }
 
                 let offset = Point::polar_to_rect(LENGTH, 1.0 * PI / 10.0);
-                let tile = Tile::build_kite(7.0, x_center + offset.0, y_center + offset.1);
-                tiles.push(tile);
-                let tile = Tile::build_kite(-1.0, x_center - offset.0, y_center + offset.1);
-                tiles.push(tile);
+                let tile = Tile::build_tile(7.0, x_center + offset.0, y_center + offset.1, true);
+                tiles.extend(tile);
+                let tile = Tile::build_tile(-1.0, x_center - offset.0, y_center + offset.1, true);
+                tiles.extend(tile);
             }
         };
         tiles
@@ -181,6 +229,7 @@ pub struct Exports {
     state: State,
     variant: Variant,
     tiles: Vec<Tile>,
+    iterations: u32,
 }
 
 impl Exports {
@@ -201,11 +250,14 @@ impl Exports {
         let x_center = w / 2.0 + OFFSET;
         let y_center = h / 2.0 + OFFSET;
 
+        let iterations = 5;
+
         let tiles = variant.start_tiles(x_center, y_center);
         Self {
             state: State::Setup,
             variant,
             tiles,
+            iterations,
         }
     }
 }
@@ -233,7 +285,17 @@ impl Algorithm for Exports {
             _ => {}
         }
 
-        let done = true;
+        let mut done = false;
+
+        let mut next = vec![];
+        for tile in &self.tiles {
+            next.extend(tile.subdivide());
+        }
+        self.tiles = next;
+        self.iterations -= 1;
+        if self.iterations == 0 {
+            done = true;
+        }
 
         if done {
             self.state = State::Done;
@@ -242,21 +304,23 @@ impl Algorithm for Exports {
     }
 
     fn draw(&self) {
-        // draw_board(self.grid);
-        let x = OFFSET;
-        let y = OFFSET;
-        let w = COLUMNS * CELL_WIDTH;
-        let h = ROWS * CELL_WIDTH;
-        draw_rectangle_lines(x, y, w, h, LINE_WIDTH, COLORS[0]);
-
-        let x_center = w / 2.0 + x;
-        let y_center = h / 2.0 + y;
-
         for tile in &self.tiles {
             tile.draw();
         }
 
-        draw_circle(x_center, y_center, 3.0, COLORS[1]);
+        let x = OFFSET;
+        let y = OFFSET;
+        let w = COLUMNS * CELL_WIDTH;
+        let h = ROWS * CELL_WIDTH;
+        draw_rectangle(0.0, 0.0, x, h, WHITE);
+        draw_rectangle(0.0, 0.0, w, y, WHITE);
+        draw_rectangle(x + w, 0.0, x, h, WHITE);
+        draw_rectangle(0.0, y + h, w, y, WHITE);
+        draw_rectangle_lines(x, y, w, h, LINE_WIDTH, COLORS[0]);
+
+        // let x_center = w / 2.0 + x;
+        // let y_center = h / 2.0 + y;
+        // draw_circle(x_center, y_center, 3.0, COLORS[1]);
 
         // if self.state == State::Running {}
     }
